be7f8277f9303e8057b2c24254915e1b3f372532
Sevenths <tuannguyen8531@gmail.com>
2024-04-21 22:32:06 +0700

    feat: add tensorflow model

--- sa_app/views.py
+++ sa_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 
 from .predict import predict
+from .tf_predict import tf_predict
 
 # Create your views here.
 
@@ -9,10 +10,17 @@ def index(request: HttpRequest):
     result = {}
     if request.method == 'POST':
         text = request.POST.get('text')
-        output, pred = predict(text)
+        model = request.POST.get('model')
+
+        if model == 'pytorch':
+            output, pred = predict(text)
+        else:
+            output, pred = tf_predict(text)
+
         result['output'] = output
         result['pred'] = pred
         result['text'] = text
+        result['model'] = model
         result['text_length'] = len(text.split())
 
     return render(request, 'index.html', result)
