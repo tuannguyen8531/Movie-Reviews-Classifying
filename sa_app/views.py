586d457d5cf19c0fddd36febc3f1ca3e8e10975c
Nguyễn Dương Anh Tuấn <83623829+tuannguyen8531@users.noreply.github.com>
2024-04-23 10:21:02 +0700

    fix: remove tensorflow.

--- sa_app/views.py
+++ sa_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpRequest
 
 from .predict import predict
-from .tf_predict import tf_predict
 
 # Create your views here.
 
@@ -10,17 +9,10 @@ def index(request: HttpRequest):
     result = {}
     if request.method == 'POST':
         text = request.POST.get('text')
-        model = request.POST.get('model')
-
-        if model == 'pytorch':
-            output, pred = predict(text)
-        else:
-            output, pred = tf_predict(text)
-
+        output, pred = predict(text)
         result['output'] = output
         result['pred'] = pred
         result['text'] = text
-        result['model'] = model
         result['text_length'] = len(text.split())
 
     return render(request, 'index.html', result)
